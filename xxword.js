f2ea9f4b3e828668404f7b4396716e46dcc2608d
Andrew Smith <asmith379@gatech.edu>
2016-12-19 15:38:09 -0600

    renders grid of cells

--- xxword.js
+++ xxword.js
@@ -11,10 +11,11 @@ function padNum(num, desiredLength) {
 	return result;
 }
 
-function initXWord(xml) {
+function initXWord(xmlString) {
 	var canvas = document.getElementById("xword");
 	var ctx = canvas.getContext("2d");
 
+	// Set up canvas position / size
 	var canvasPadding = 100;
 	canvas.style.left = canvasPadding / 2 + "px";
 	canvas.style.top = canvasPadding / 2 + "px";
@@ -23,11 +24,43 @@ function initXWord(xml) {
 
 	var puzzlePadding = 100;
 	var puzzleDimension = Math.min(ctx.canvas.width, ctx.canvas.height) - puzzlePadding
-		
-	ctx.fillStyle = "rgb(200, 0, 0)";
-	ctx.fillRect(puzzlePadding / 2, puzzlePadding / 2, puzzleDimension, puzzleDimension);
+	var puzzleX = puzzlePadding / 2;
+	var puzzleY = puzzlePadding / 2;
 	
-	// TODO: google DOPMarser
+	ctx.fillStyle = "rgb(200, 0, 0)";
+	ctx.fillRect(puzzleX, puzzleY, puzzleDimension, puzzleDimension);
+
+
+	// Set up xml parsing
+	var parser = new DOMParser();
+	var xml = parser.parseFromString(xmlString, "text/xml");
+
+	var gridXml = xml.getElementsByTagName("grid")[0]
+	var gridRows = parseInt(gridXml.getAttribute("height"));
+	var gridCols = parseInt(gridXml.getAttribute("width"));
+	
+	if(gridRows !== gridCols) {
+		alert("Retrieved invalid crossword (rows not equal to columns)\nRows: " + gridRows + "\nCols: " + gridCols);
+		return;
+	}
+
+	var gridDimension = gridRows;
+
+	var cellMargin = 3;
+	var numMargins = gridDimension + 1;
+	
+	var cellDimension = (puzzleDimension - (cellMargin * numMargins)) / gridDimension;
+
+	ctx.fillStyle = "rgb(0, 0, 0)";
+
+	for(var i = 0; i < gridDimension; i++) {
+		for(var j = 0; j < gridDimension; j++) {
+			var x = puzzleX + cellMargin + j * (cellDimension + cellMargin);
+			var y = puzzleY + cellMargin + i * (cellDimension + cellMargin);
+			
+			ctx.fillRect(x, y, cellDimension, cellDimension);
+		}
+	}
 }
 
 var xwordBaseUrl = "http://cdn.games.arkadiumhosted.com/latimes/assets/DailyCrossword/";
