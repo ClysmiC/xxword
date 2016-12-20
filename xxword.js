81b6fa00c580f4bc14c36804e411b699d13492f0
Andrew Smith <asmith379@gatech.edu>
2016-12-19 19:09:51 -0600

    added functionality for highlighting cells, drawing numbers and values

--- xxword.js
+++ xxword.js
@@ -11,9 +11,89 @@ function padNum(num, desiredLength) {
 	return result;
 }
 
+// Returns all the cells that get focused when the focus
+// is on cell X, Y with orientation either "down" or "across".
+// The cells returned are in order, from first letter of the word
+// to the last letter
+function getFocusedCells(puzzle, x, y, orientation) {
+	var result = [];
+	
+	var cells = puzzle.cells;
+	
+	if(orientation !== "down" && orientation !== "across") {
+		return [];
+	}
+
+	if(cells[y][x].solution === "#") {
+		return [];
+	}
+
+	var deltaX = 0;
+	var deltaY = 0;
+
+	if(orientation === "down") {
+		deltaY = 1;
+	}
+	else {
+		deltaX = 1;
+	}
+
+	// Iterate backwards until at the first letter of the word
+	while(x - deltaX >= 0 && y - deltaY >= 0 && cells[y - deltaY][x - deltaX].solution !== "#") {
+		x -= deltaX;
+		y -= deltaY;
+	}
+
+	// Iterate forwards and add each cell to our list
+	while(x < puzzle.gridDimension && y < puzzle.gridDimension && cells[y][x].solution !== "#") {
+		result.push(cells[y][x]);
+		
+		x += deltaX;
+		y += deltaY;
+	}
+
+	return result;
+}
+
+function drawCellNumber(puzzle, cell) {
+	if(cell.number !== -1) {
+		puzzle.ctx.font = puzzle.numberFont;
+		puzzle.ctx.fillStyle = "rgb(0, 0, 0)";
+		puzzle.ctx.textAlign = "start";
+		
+		puzzle.ctx.fillText(cell.number, cell.canvasX + 2, cell.canvasY + puzzle.numberFontHeight + 2);
+	}
+}
+
+function drawCellValue(puzzle, cell) {
+	if(cell.value !== "") {
+		puzzle.ctx.font = puzzle.valueFont;
+		puzzle.ctx.fillStyle = "rgb(0, 0, 0)";
+		puzzle.ctx.textAlign = "center";
+		
+		puzzle.ctx.fillText(cell.value, cell.canvasX + puzzle.cellDimension / 2, cell.canvasY + puzzle.cellDimension / 2 + puzzle.valueFontHeight / 2);
+	}
+}
+
+// note: call this with color = "rgb(255, 255, 255)" to "clear" highlights
+function highlightCells(puzzle, cells, color) {
+	for (var i = 0; i < cells.length; i++) {
+		var cell = cells[i];
+		
+		puzzle.ctx.fillStyle = color;
+		puzzle.ctx.fillRect(cell.canvasX, cell.canvasY, puzzle.cellDimension, puzzle.cellDimension);
+		
+		drawCellNumber(puzzle, cell);
+		drawCellValue(puzzle, cell);
+	}
+}
+
 function initXWord(xmlString) {
+	var puzzle = {};
 	var canvas = document.getElementById("xword");
 	var ctx = canvas.getContext("2d");
+
+	puzzle.ctx = ctx; // store copy here that we can use when we pass puzzle around to functions
 
 	// Set up canvas position / size
 	var canvasPadding = 100;
@@ -30,7 +110,6 @@ function initXWord(xmlString) {
 	ctx.fillStyle = "rgb(0, 0, 0)";
 	ctx.fillRect(puzzleX, puzzleY, puzzleDimension, puzzleDimension);
 
-
 	// Set up xml parsing
 	var parser = new DOMParser();
 	var xml = parser.parseFromString(xmlString, "text/xml");
@@ -40,24 +119,24 @@ function initXWord(xmlString) {
 	var gridCols = parseInt(gridXml.getAttribute("width"));
 	
 	if(gridRows !== gridCols) {
-		alert("Retrieved invalid crossword (rows not equal to columns)\nRows: " + gridRows + "\nCols: " + gridCols);
+		alert("ERROR: Retrieved invalid crossword (rows not equal to columns)\nRows: " + gridRows + "\nCols: " + gridCols);
 		return;
 	}
 
-	var gridDimension = gridRows;
+	puzzle.gridDimension = gridRows;
 
 	// cache xml element for each cell in 'cells'
 	// cells is accessed via cells[row][column]
-	var cells = new Array(gridDimension);
-	for(var i = 0; i < cells.length; i++) {
-		cells[i] = new Array(gridDimension);
+	puzzle.cells = new Array(puzzle.gridDimension);
+	for(var i = 0; i < puzzle.cells.length; i++) {
+		puzzle.cells[i] = new Array(puzzle.gridDimension);
 	}
 
-	for(var i = 0; i < gridDimension; i++) {
-		for(var j = 0; j < gridDimension; j++) {
+	for(var i = 0; i < puzzle.gridDimension; i++) {
+		for(var j = 0; j < puzzle.gridDimension; j++) {
 			// the xml iterates in column major order, but cells is in row major order,
 			//which is why j and i may seem backwards in the math below
-			var cellXml = gridXml.children[1 + j * gridDimension + i];
+			var cellXml = gridXml.children[1 + j * puzzle.gridDimension + i];
 			
 			var solution;
 			if(cellXml.hasAttribute("type") && cellXml.getAttribute("type") === "block") {
@@ -75,42 +154,42 @@ function initXWord(xmlString) {
 				number = -1;
 			}			
 			
-			cells[i][j] = {
+			puzzle.cells[i][j] = {
 				row: i,
 				column: j,
 				solution: solution,
 				number: number,
+				value: solution, //"",   // user-entered value
 			};
 		}
 	}
 
 	// Draw cells
 	var cellMargin = 3;
-	var numMargins = gridDimension + 1;
+	var numMargins = puzzle.gridDimension + 1;
 	
-	var cellDimension = (puzzleDimension - (cellMargin * numMargins)) / gridDimension;
+	puzzle.cellDimension = (puzzleDimension - (cellMargin * numMargins)) / puzzle.gridDimension;
 	
-	var fontHeight = (cellDimension / 3.5);
-	ctx.font = fontHeight + "px sans-serif";
+	puzzle.numberFontHeight = (puzzle.cellDimension / 4);
+	puzzle.numberFont = puzzle.numberFontHeight + "px sans-serif";
 
-	for(var i = 0; i < cells.length; i++) {
-		for(var j = 0; j < cells[0].length; j++) {
-			var cell = cells[i][j];
+	puzzle.valueFontHeight = (puzzle.cellDimension * .75);
+	puzzle.valueFont = puzzle.valueFontHeight + "px sans-serif";
+	
+	for(var i = 0; i < puzzle.cells.length; i++) {
+		for(var j = 0; j < puzzle.cells[0].length; j++) {
+			var cell = puzzle.cells[i][j];
 
-			cell.canvasX = puzzleX + cellMargin + j * (cellDimension + cellMargin);
-			cell.canvasY = puzzleY + cellMargin + i * (cellDimension + cellMargin);
+			cell.canvasX = puzzleX + cellMargin + j * (puzzle.cellDimension + cellMargin);
+			cell.canvasY = puzzleY + cellMargin + i * (puzzle.cellDimension + cellMargin);
 
 			if(cell.solution !== "#") {
-				ctx.fillStyle = "rgb(255, 255, 255)";
-				ctx.fillRect(cell.canvasX, cell.canvasY, cellDimension, cellDimension);
-
-				if(cell.number !== -1) {
-					ctx.fillStyle = "rgb(0, 0, 0)";
-					ctx.fillText(cell.number, cell.canvasX + 2, cell.canvasY + fontHeight + 2);
-				}
+				highlightCells(puzzle, [cell], "rgb(255, 255, 255)");
 			}
 		}
 	}
+
+	highlightCells(puzzle, getFocusedCells(puzzle, 7, 0, "across"), "rgb(255, 255, 127)");
 }
 
 var xwordBaseUrl = "http://cdn.games.arkadiumhosted.com/latimes/assets/DailyCrossword/";
